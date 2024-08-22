49aef09e0bd9368ca9813eae6eb53cf92d13bea8
Abdulsalam Abdulsalam <0xdudul@gmail.com>
2024-08-22 17:24:33 +0000

    feat: Simple contract implemented.

---
+++ auto.bash
@@ -0,0 +1,18 @@
+npm run compile
+
+npm run deploy
+npm run deploy
+npm run deploy
+
+npm run set-message
+npm run set-message
+npm run set-message
+
+npm run get-message
+npm run get-message
+
+rm -rf .git
+git init
+git add .
+git commit -m "feat: Simple contract implemented."
+
---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+const deployedAddress = '0x9d6C39A7F73923b669929AC70dEF5876c5E6D069'
+
+export default deployedAddress
